1f91b5004d88e2e99679768fc6701d0332e87566
melonhong <melonhong46@gmail.com>
2025-10-03 16:37:24 +0900

    feat: refresh token 생성 및 DB 요청 코드 추가
    - AuthServiceImpl
    - 일반 로그인과 같이 카카오 로그인 시에도 refresh token을 처리할 수 있도록 구현함.
    - javadoc 주석도 추가함.
    
    - RefreshTokenRequest
    - 카카오 로그인 시 사용할 수 있도록 @AllArgsConstructor 어노테이션을 추가함.

--- src/main/java/com/cluvy/auth/dto/RefreshTokenRequest.java
+++ src/main/java/com/cluvy/auth/dto/RefreshTokenRequest.java
@@ -2,9 +2,11 @@ package com.cluvy.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class RefreshTokenRequest {
     @NotBlank
     private String refreshToken;
