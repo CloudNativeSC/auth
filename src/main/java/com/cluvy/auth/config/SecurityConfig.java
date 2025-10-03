845802f1a172a57325e999e1a279952d3f2bc7c5
melonhong <melonhong46@gmail.com>
2025-10-03 16:14:16 +0900

    feat: 임시 CORS 관련 코드
    - 테스트를 위해 임시적으로 CORS 정책에 위반되지 않도록 설정함.
    - 추후에 변경될 가능성 존재.

--- src/main/java/com/cluvy/auth/config/SecurityConfig.java
+++ src/main/java/com/cluvy/auth/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -18,6 +20,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                // 임시로 만든 CORS 허용 코드
+                .cors(cors -> cors.configurationSource(request -> {
+                    var corsConfig = new org.springframework.web.cors.CorsConfiguration();
+                    corsConfig.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트 주소
+                    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                    corsConfig.setAllowedHeaders(List.of("*"));
+                    corsConfig.setAllowCredentials(true); // 쿠키 허용
+                    return corsConfig;
+                }))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/api/auth/**", "/oauth/**").permitAll()
